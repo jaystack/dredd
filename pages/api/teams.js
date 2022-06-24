36aad2ed0066fc915b53b4aae4cf038da0f60e4b
PeterAronZentai <peter.zentai@jaystack.com>
2022-06-24 09:12:54 +0000

    initial

---
+++ pages/api/teams.js
@@ -0,0 +1,15 @@
+const teams = [
+    { team: 'Parrot', cluster: 'A' },
+    { team: 'Bear', cluster: 'A' },
+    { team: 'Eagle', cluster: 'A' },
+    { team: 'Raccoon', cluster: 'A' },
+    { team: 'Fox', cluster: 'A' },
+    { team: 'Frog', cluster: 'A' },
+
+    { team: 'Peacock', cluster: 'A' },
+    { team: 'Bear', cluster: 'A' },
+    { team: 'Eagle', cluster: 'A' },
+    { team: 'Raccoon', cluster: 'A' },
+    { team: 'Fox', cluster: 'A' },
+    { team: 'Frog', cluster: 'A' }
+]   
